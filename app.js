6b95272cb648d3caf14973294940dcc66a0d452e
Ramanpreet Nara <ramanpreetnara@gmail.com>
2014-08-17 22:06:06 -0400

    User can now freely send messages to the server.

--- app.js
+++ app.js
@@ -28,9 +28,15 @@ app.use(require('./routes/index'));
 
 // socket.io 
 io.on('connection', function(socket) {
+    
     console.log('user connected!');
+    
     socket.on('disconnect', function() {
         console.log('user disconnected!');
+    })
+
+    socket.on('message', function(data){
+        console.log(data);
     })
 })
 
