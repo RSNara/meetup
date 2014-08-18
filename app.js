8b1dee5d148b4615046e7d310c4cf13e8efda5e4
Ramanpreet Nara <ramanpreetnara@gmail.com>
2014-08-17 20:39:01 -0400

    Removing deprecated static-favicon for the new serve-favicon module.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -15,10 +15,10 @@ var server = require(path.join(__dirname, 'bin/www'))(app, 9000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded( { extended:true } ));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
