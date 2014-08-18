8dbb1acdb5ebf2c1d91e69a8e9a35808f627cbf1
Ramanpreet Nara <ramanpreetnara@gmail.com>
2014-08-17 21:26:33 -0400

    Removing all user routes and initializing the chat routes/code.

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var chat = require('./routes/chat');
 
 var app = express();
 var server = require(path.join(__dirname, 'bin/www'))(app, 9000);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +22,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
+// route setup
+app.use('/chat', chat);
 app.use('/', routes);
-app.use('/users', users);
+
+// socket.io 
+io.on('connection', function(socket) {
+    console.log('user connected!');
+    socket.on('disconnect', function() {
+        console.log('user disconnected!');
+    })
+})
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
