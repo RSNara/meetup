f5270d4b8225d043aad4b7088293a714631df587
Ramanpreet Nara <ramanpreetnara@gmail.com>
2014-08-24 15:18:25 -0400

    Making it so that if a user tries to create an already existing chat room, he'll be redirected to it. Previously, it would do nothing.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -3,7 +3,8 @@ $(function(){
 	$("form#meetup > input#public").click(function (e){
 		roomLocation = "/public/" + $("form#meetup > input#roomName").val();
 		$.post(roomLocation, function (data) {
-			if (JSON.parse(data).successful) {
+			response = JSON.parse(data);
+			if (response.successful || response.exists) {
 				window.location.href = roomLocation;
 			}
 		});
--- routes/public.js
+++ routes/public.js
@@ -4,24 +4,30 @@ var mongoose = require('mongoose');
 var router = express.Router();
 var Room = mongoose.model('Room');
 
+// make sure that if the room already exists, we simply redirect the user
 router.post('/:room', function (request, response) {
-	Room.create({ name: request.params.room }, function(error, docs) {
-		if (! error) {
-			status = { successful: true };
-		} else {
-			status = { successful: false };
+	
+	Room.findOne({ name: request.params.room }, function (error, doc) {
+
+		if (! doc) {
+			Room.create({ name: request.params.room }, function (error, docs) {
+				var status = (! error) ? { successful: true } : { successful: false };
+				response.end(JSON.stringify(status));
+			});
+		} 
+		else {
+			response.end(JSON.stringify({ exists: true }));
 		}
-		
-		response.end(JSON.stringify(status));
 	});
+
 })
 
 router.get('/:room', function (request, response) {
 	Room.findOne({ name: request.params.room }, function (error, doc) {
-		if (! doc) {
-			response.redirect('/');
+		if (! doc) { 
+			response.redirect('/'); 
 		} else {
-			response.render('public', { name: request.params.room} );
+			response.render('public', { name: request.params.room});
 		}
 	});
 })
@@ -29,7 +35,7 @@ router.get('/:room', function (request, response) {
 router.get('*', function (request, response) {
 	Room.find({}, function (error, docs) {
 		response.end(JSON.stringify(docs));
-	})
+	});
 })
 
 module.exports = router;
