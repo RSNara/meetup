b5fef16804071664b7c24b2934d8481dd2ced8ed
Ramanpreet Nara <ramanpreetnara@gmail.com>
2014-08-25 22:13:04 -0400

    Getting the message bar to look prettier.

--- public/javascripts/chats.js
+++ public/javascripts/chats.js
@@ -6,26 +6,25 @@ function handleChats(namespace, room) {
 		$("#chatHistory").append("<div>" + message.author + ": " + message.body + "</div>")
 	}
 
-	$("form > button#send").one('click', function () {
+	$("form button#send").one("click", function () {
 
 		socket.emit("join request", {
 			room: room,
-			name: $("form > input#message").val()
+			name: $("form input#message").val()
 		});
 
-		$("form > button#send").html("Send");
-		$("form > label").html($("form > input#message").val() + ": ");
-		$("form > input#message").val('');
+		$("form button#send").html("Send");
+		$("form input#message").val("").attr("placeholder", "Enter message");
 
-		$("form > button#send").click(function (e){
-			socket.emit('message', $("form > input#message").val());
-			$("form > input#message").val('');
+		$("form button#send").click(function (e){
+			socket.emit('message', $("form input#message").val());
+			$("form input#message").val('');
 		})
 
 	})
 
 	$(document).keypress(function (e){
-	    (e.which === 13) && $("form > button#send").click();
+	    (e.which === 13) && $("form button#send").click();
 	})
 
 	socket.on("message", function (message){
